64ad11a0c897dd5ff67d713d1a5e7987ffdcd294
akaDova <miraak.dova@gmail.com>
2017-12-02 17:57:47 +0300

    fix display metrics

--- code.rb
+++ code.rb
@@ -8,6 +8,18 @@ until abs( vs) < EPS
   vs = - vs * x * x / (2 * n - 1) / (2 * n - 2)
   n = n + 1
   y = y + vs
+  op()
   str = "kjoo"
+  myTest()
+  myTest()
+
 end
 puts( x , y , EPS , "qwerty" )
+
+def op
+  v = 5 + p
+end
+
+def myTest
+  test = 6
+end
--- halstead_metric.rb
+++ halstead_metric.rb
@@ -31,7 +31,6 @@ class HalsteadMetric
   def calculate
     @operands = {}
     @operators = {}
-    brackets = []
     @code_text.select {|line| !@@IGNORE_WORDS.find {|word| /^.*\b#{word}\b.*$/ === line}}.each do |line|
       (
         @operands[line[/".*"/]] = 0 if @operands[line[/".*"/]].nil?
@@ -39,7 +38,6 @@ class HalsteadMetric
         line.sub!(/".*"/, " ")
       ) while /^.*".*".*$/ === line
       @@RES_WORDS.each do |word|
-        words_line = line
         (
           @operators[word] = 0 if @operators[word].nil?
           @operators[word] += 1
@@ -47,7 +45,6 @@ class HalsteadMetric
         ) while /^.*\b#{Regexp.escape word}\b.*$/ === line
       end
       @@PURE_OPS.each do |op|
-        ops_line = line
         (
           @operators[op] = 0 if @operators[op].nil?
           @operators[op] += 1
@@ -55,7 +52,6 @@ class HalsteadMetric
         ) while /^.*\s#{Regexp.escape op}\s.*$/ === line
       end
       @@BRACKETS.each do |br|
-        br_line = line
         (
           @operators[br] = 0 if @operators[br].nil?
           @operators[br] += 1
@@ -80,18 +76,16 @@ class HalsteadMetric
     @N2 = @operands.to_a.inject(0) {|num, val| num + val[1]}
     @N = @N1 + @N2
     @v = @N * Math.log2(@n)
-
   end
   private :calculate
 
   def show
     size  = @operands.length > @operators.length ? @operands.length : @operators.length;
     printf("%3s  %15s   %3s  | %3s   %15s  %3s\n", "j", "operators", "f1j", "i", "operand", "f2i")
-
     size.times do |i|
-      #printf("j  operator  f1j  i   operand   f2i")
       printf("--------------------------------------------------------\n")
       printf("%3d  %15s   %3d  | ", i + 1, @operators.to_a[i][0], @operators.to_a[i][1]) if i < @operators.length
+      printf("%3s  %15s   %3s  | ", "", "", "") if i >= @operators.length
       printf("%3d   %15s  %3d", i + 1, @operands.to_a[i][0], @operands.to_a[i][1]) if i < @operands.length
       printf("\n")
     end
